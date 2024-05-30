1ad7c0ad64c46ceff9eea77e7f80b4a837275f18
cccmmmd <bblisme8b@gmail.com>
2024-05-30 16:37:23 +0800

    add error page and fix navbar bugs

---
+++ error.vue
@@ -0,0 +1,10 @@
+<script setup>
+    defineProps(['error'])
+</script>
+<template>
+    <div class="w-full h-screen bg-gradient-to-r to-emerald-600 from-sky-400 flex flex-col justify-center items-center">
+        <h1 class="mb-4 text-3xl font-extrabold text-white dark:text-white md:text-5xl lg:text-6xl">頁面發生錯誤 </h1>
+        <div class="text-lg font-normal text-gray-900 lg:text-xl dark:text-gray-400">{{ error.message }}</div>
+    </div>
+</template>
+
--- layouts/default.vue
+++ layouts/default.vue
@@ -15,20 +15,20 @@
                 <div class="hidden w-full md:block md:w-auto" id="navbar-default">
                 <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                     <li>
-                    <NuxtLink to="/" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" :class="{ 'text-blue-600': $route.path === '/' }" >Home</NuxtLink>
+                    <NuxtLink to="/" class="block py-2 px-3  rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" :class="{ 'text-blue-600': $route.path === '/' }" >Home</NuxtLink>
                     </li>
                     <li>
-                    <NuxtLink to="/search" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" :class="{ 'text-blue-600': $route.path === '/search' }">Search</NuxtLink>
+                    <NuxtLink to="/search" class="block py-2 px-3  rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" :class="{ 'text-blue-600': $route.path === '/search' }">Search</NuxtLink>
                     </li>
                     <li>
-                    <NuxtLink to="/popular" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" :class="{ 'text-blue-600': $route.path === '/popular' }">Popular</NuxtLink>
+                    <NuxtLink to="/popular" class="block py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent" :class="{ 'text-blue-600': $route.path === '/popular' }">Popular</NuxtLink>
                     </li>
                     <li>
-                    <NuxtLink to="/now-playing" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                    <NuxtLink to="/now-playing" class="block py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                     :class="{ 'text-blue-600': $route.path === '/now-playing' }">Now Playing</NuxtLink>
                     </li>
                     <li>
-                    <NuxtLink to="/top-rated" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                    <NuxtLink to="/top-rated" class="block py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                     :class="{ 'text-blue-600': $route.path === '/top-rated' }">Top Rated</NuxtLink>
                     </li>
                     <li>
